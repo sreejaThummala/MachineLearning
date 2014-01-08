472b6756f9531acf22a0ca00f537fb8cc2193f08
Sreeja Thummala <sthummala@wisc.edu>
2014-01-08 14:30:37 -0500

    Create KNNResult.java

---
+++ KNNClustering/KNNResult.java
@@ -0,0 +1,13 @@
+
+
+public class KNNResult {
+	
+	// The accuracy of your kNN algorithm in accordance with k
+	public double accuracy;
+	
+	// The category assignment of each test instance, 
+	public String[] categoryAssignment;
+	
+	// The assignment of nearest neighbors for test instances
+	public String[][] nearestNeighbors;
+}
